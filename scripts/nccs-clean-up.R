393c2e137e0f6deb62930352583a6cb0fc1555ef
vulpes37 <rjlevitt37@gmail.com>
2020-06-19 09:58:24 -0600

    subset to orleans

---
+++ scripts/nccs-clean-up.R
@@ -0,0 +1,52 @@
+# clear environment
+rm(list=ls())
+
+
+# INSTALL ANY NEEDED PACKAGES----
+#install.packages('reshape2')
+library(reshape2)
+
+# SET YOUR DIRECTORY ----
+cd <- "~/social-capital"
+cd_data <- paste(cd, "data", sep=.Platform$file.sep)
+setwd(cd_data)
+# **********************************************************************
+
+# load data 
+
+# get list of files names 
+fileNames <- list.files(pattern = "\\.csv$")
+
+# load first file 
+start_i <- 1
+dat <- read.csv( fileNames[start_i], stringsAsFactors = FALSE )
+
+# year 
+year <- as.integer( substr(fileNames[start_i], 10, 13) )
+dat$file_year <- year
+
+# delete mising
+dat <- dat[ !(is.na(dat$FIPS)), ]
+dat <- dat[dat$FIPS == 22071,  c("EIN", "FIPS", "STYEAR", "file_year", "NTEE1", "TOTREV", "ZIP", "ZIP5")]
+
+for(i in 2:length(fileNames)){
+  
+  # index 
+  print( paste("just loaded", substr(fileNames[i], 10, 13), sep = " ") )
+  print( paste(i, paste( "out of", length(fileNames), sep = " "), sep = " ") )
+  
+  # load
+  temp <- read.csv( fileNames[i], stringsAsFactors = FALSE )
+  
+  # year 
+  year <- as.integer( substr(fileNames[start_i], 10, 13) )
+  temp$file_year <- year
+  
+  # delete mising
+  temp <- temp[ !(is.na(temp$FIPS)), ]
+  temp <- temp[temp$FIPS == 22071,  c("EIN", "FIPS", "STYEAR", "file_year", "NTEE1", "TOTREV", "ZIP", "ZIP5")]
+  
+  # bind with full set
+  dat <- rbind(dat, temp)
+}
+  
