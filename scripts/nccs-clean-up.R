f66c4a5461a39a3b1b79bc5f4c837348341a0ac5
vulpes37 <rjlevitt37@gmail.com>
2020-06-19 15:30:44 -0600

    minor

--- scripts/nccs-clean-up.R
+++ scripts/nccs-clean-up.R
@@ -27,7 +27,7 @@ dat$file_year <- year
 
 # delete mising
 dat <- dat[ !(is.na(dat$FIPS)), ]
-dat <- dat[dat$FIPS == 22071,  c("EIN", "FIPS", "STYEAR", "file_year", "NTEE1", "TOTREV", "ZIP", "ZIP5")]
+dat <- dat[dat$FIPS == 22071,  c("EIN", "FIPS", "STYEAR", "file_year", "NTEE1", "TOTREV", "ZIP")]
 
 for(i in 2:length(fileNames)){
   
@@ -37,16 +37,23 @@ for(i in 2:length(fileNames)){
   
   # load
   temp <- read.csv( fileNames[i], stringsAsFactors = FALSE )
-  
+
   # year 
-  year <- as.integer( substr(fileNames[start_i], 10, 13) )
+  year <- as.integer( substr(fileNames[i], 10, 13) )
   temp$file_year <- year
   
-  # delete mising
-  temp <- temp[ !(is.na(temp$FIPS)), ]
-  temp <- temp[temp$FIPS == 22071,  c("EIN", "FIPS", "STYEAR", "file_year", "NTEE1", "TOTREV", "ZIP", "ZIP5")]
-  
+  if(year==2004){
+    # delete mising
+    temp <- temp[ !(is.na(temp$Fips)), ]
+    temp <- temp[temp$FIPS == 22071,  c("EIN", "Fips", "STYEAR", "file_year", "NTEE1", "TOTREV", "ZIP")]
+  }else{
+    # delete mising
+    temp <- temp[ !(is.na(temp$FIPS)), ]
+    temp <- temp[temp$FIPS == 22071,  c("EIN", "FIPS", "STYEAR", "file_year", "NTEE1", "TOTREV", "ZIP")]
+  }
+
   # bind with full set
   dat <- rbind(dat, temp)
 }
-  
+
+write.csv("nccs_orleans.csv", row.names = FALSE)  
