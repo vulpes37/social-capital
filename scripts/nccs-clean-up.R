1d81e42cbdf324fa03e163916a861cd89ff6fac4
slootmaker <chris.slootmaker@mountaindatagroup.com>
2020-06-19 15:02:07 -0600

    compare sizes

--- scripts/nccs-clean-up.R
+++ scripts/nccs-clean-up.R
@@ -4,6 +4,7 @@ rm(list=ls())
 
 # INSTALL ANY NEEDED PACKAGES----
 #install.packages('reshape2')
+library(readstata13)
 library(reshape2)
 
 # SET YOUR DIRECTORY ----
@@ -50,3 +51,19 @@ for(i in 2:length(fileNames)){
   dat <- rbind(dat, temp)
 }
   
+
+compare_fwf_dta <- function() {
+  
+  dta <- read.dta13("la_births_deaths/LA_Births.dta")
+  bf_widths <- read.csv("la_births_deaths/B9909_widths.csv")
+  bf <- read.fwf("la_births_deaths/B9909", widths=bf_widths["width"])
+  print( paste('LA_Births.dta shape:', nrow(dta), 'rows,', ncol(dta), 'cols.', sep = " ") )
+  print( paste('B9909.txt shape:', nrow(bf), 'rows,', ncol(bf), 'cols.', sep = " ") )
+  
+} 
+
+# compare dta vs. B9909 - warning: loading fwf takes around an hour.
+# compare_fwf_dta()
+# "LA_Births.dta shape: 725534 rows, 141 cols."
+# "B9909.txt shape: 725534 rows, 158 cols."
+
